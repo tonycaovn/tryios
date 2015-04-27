66db0b380778cb0d3bc936979b178ec81ee2b879
Thanh <ctt.bk.hcmut2009@gmail.com>
2015-04-27 22:35:10 +0700

    Initial Commit

---
+++ luytxuyt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  luytxuyt
+//
+//  Created by tonycao on 4/27/15.
+//  Copyright (c) 2015 Thanh Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ luytxuyt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  luytxuyt
+//
+//  Created by tonycao on 4/27/15.
+//  Copyright (c) 2015 Thanh Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
